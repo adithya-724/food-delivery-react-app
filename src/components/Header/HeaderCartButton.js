b7bf28492ef0f02e10dbee8c0f6a2f5c0e7e2c36
adithya-724 <adithyamadhu2@gmail.com>
2022-02-03 01:29:46 +0530

    used use effect hook for highlighting whenever an item is added to the cart

--- src/components/Header/HeaderCartButton.js
+++ src/components/Header/HeaderCartButton.js
@@ -1,13 +1,28 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { FiShoppingCart } from "react-icons/fi";
 import styles from "./HeaderCartButton.module.css";
 import { cartContext } from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(cartContext);
-  const numberOfCartItems = cartCtx.items.reduce((index, item) => {
+  const { items } = cartCtx;
+  const [isHighlighted, setIsHighlighted] = useState(false);
+  const numberOfCartItems = items.reduce((index, item) => {
     return index + item.amount;
   }, 0);
+  const classes = `${styles.items} ${isHighlighted ? styles.change : ""}`;
+  useEffect(() => {
+    if (items.length > 0) {
+      setIsHighlighted(true);
+    }
+    const timer = setTimeout(() => {
+      setIsHighlighted(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
     <>
       <button className={styles.btn} onClick={props.onClick}>
@@ -17,7 +32,7 @@ const HeaderCartButton = (props) => {
           </span>
           <span className={styles.text}>Cart</span>
         </span>
-        <span className={styles.items}>{numberOfCartItems}</span>
+        <span className={classes}>{numberOfCartItems}</span>
       </button>
     </>
   );
